341b4e258539e528ba67ca8bfa32390edc0aa1cc
ramonparaiba <ramonparaiba@gmail.com>
2018-02-13 00:38:58 -0300

    Classe de Domínio Categoria - Testada

--- src/main/java/com/nidigitalstudio/aulawebpablo/resources/CategoriaResource.java
+++ src/main/java/com/nidigitalstudio/aulawebpablo/resources/CategoriaResource.java
@@ -1,15 +1,27 @@
 package com.nidigitalstudio.aulawebpablo.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.nidigitalstudio.aulawebpablo.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest está funfando, chimpa!";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1, "informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}	
 }
